97c2c6afd39a1031be3ca940c921f506e42e48d8
althaaf <althaaf@LAPTOP-09LJ77LS>
2025-08-19 16:34:41 +0530

    as1

--- C2TC_CoreJava_Assignments/src/assignment1/employee/Employee.java
+++ C2TC_CoreJava_Assignments/src/assignment1/employee/Employee.java
@@ -1,5 +1,46 @@
 package assignment1.employee;
 
+/**
+ * Base class representing a generic Employee.
+ */
 public class Employee {
+    // Private attributes
+    private String name;
+    private int employeeId;
+    private double salary;
 
+    // Constructor
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    protected void setSalary(double salary) { // Protected: accessible in subclasses
+        this.salary = salary;
+    }
+
+    // Display method
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", ID: " + employeeId + ", Salary: " + salary);
+    }
 }
+
