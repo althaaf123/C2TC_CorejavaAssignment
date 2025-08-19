735c6a6aa41b938e0ab4d9e5f0a4d7842c8a8d48
althaaf <althaaf@LAPTOP-09LJ77LS>
2025-08-19 19:50:35 +0530

    Ass2

---
+++ C2TC_CoreJava_Assignments/src/assignment2/Assignment2Main.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+public class Assignment2Main {
+    public static void main(String[] args) {
+        // Test Student class
+        Student s1 = new Student();
+
+        // Test Commission class
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+    }
+}
---
+++ C2TC_CoreJava_Assignments/src/assignment2/Student.java
@@ -0,0 +1,9 @@
+package assignment2;
+
+public class Student {
+    
+    // Default constructor
+    public Student() {
+        System.out.println("Student object is created");
+    }
+}
